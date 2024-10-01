8f1b1f1ae2d434694040f581b6a03d618844ec06
marvellousB <basaranmerve00@gmail.com>
2024-10-01 12:38:52 +0300

    deneme iki

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int sum_of_integers(int n) {
+	int sum = 0;
+	int i = 1;
+
+	
+	while (i <= n) {
+		sum += i;
+		i++;
+	}
+	return sum;
+}
+
 
 int main() 
 
@@ -14,6 +26,16 @@ int main()
 		}
 		
 		cout << "complexity is O(n) and n is :"<<n << endl;
+
+
+/*question1 part B*/
+		
+		int result = sum_of_integers(n);
+		cout << "The sum of the first " << n << " consecutive positive integers is: " << result << endl;
+		cout << "complexity is O(n) and n is :" << n << endl;
+
+		
+		
 
 
 
