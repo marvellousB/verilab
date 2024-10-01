b095f5765ab9cad04522d70bc63d1d8ffa9c880e
marvellousB <basaranmerve00@gmail.com>
2024-10-01 11:41:14 +0300

    deneme

---
+++ Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+
+{/*question1 part A*/
+	int n;
+	cout << "enter n :";
+	cin >> n;
+	
+		for (int i = n; i > 0; --i) {
+			cout << i << endl;
+		}
+		
+		cout << "complexity is O(n) and n is :"<<n << endl;
+
+
+
+
+
+
+}
